6f6f6731b4cf1b6a3679a428559994d709e160a7
dehughes21 <dehughes21@coe.edu>
2022-10-27 10:07:16 -0500

    Photoelectric Effect Edits

---
+++ Density Depletion Project/Magnetic Dipole.py
@@ -0,0 +1,10 @@
+from BaseFunctions import *
+
+path = "Magnetic Dipole.py"
+
+
+def exp1():
+    height = int(input("Enter height value for plot: "))
+    width = int(input("Enter width value for plot"))
+    sheet = "Sheet1"
+    radius
