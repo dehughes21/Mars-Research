83a588c0da275d47da42e6a74f8869e57c871295
dehughes21 <dehughes21@coe.edu>
2022-11-08 21:34:00 -0600

    Blackbody project added

--- Density Depletion Project/Blackbody Radiation.py
+++ Density Depletion Project/Blackbody Radiation.py
@@ -4,6 +4,22 @@ import numpy as np
 path = "BlackBody Radiation.xlsx"
 
 
+def best_fit(X, Y):
+    xbar = sum(X) / len(X)
+    ybar = sum(Y) / len(Y)
+    n = len(X)  # or len(Y)
+
+    numer = sum([xi * yi for xi, yi in zip(X, Y)]) - n * xbar * ybar
+    denum = sum([xi ** 2 for xi in X]) - n * xbar ** 2
+
+    b = numer / denum
+    a = ybar - b * xbar
+
+    print('best fit line:\ny = {:.2f} + {:.2f}x'.format(a, b))
+
+    return a, b
+
+
 def Power(dataPath):
     height = int(input("Enter height value for plot image: "))
     width = int(input("Enter width value for plot image: "))
@@ -13,17 +29,20 @@ def Power(dataPath):
     tempFourth = []
     for val in temps:
         fourth = val ** 4
-        tempFourth.append(val)
+        tempFourth.append(fourth)
 
     peakW = columnData(dataPath, sheet, 'C')
     power = columnData(dataPath, sheet, 'D')
-
+    a, b = best_fit(tempFourth, power)
+    yfit = [a + b * xi for xi in tempFourth]
     fig, ax = plt.subplots(nrows=1)
     fig.set_figheight(height)
     fig.set_figwidth(width)
     ax.set_xlabel("Temperature ($K^4$)")
     ax.set_ylabel("Power (rel*nm)")
-    ax.plot(tempFourth, power)
+    ax.scatter(tempFourth, power)
+    ax.plot(tempFourth, yfit)
+    ax.text(0.8*(10**14), 275, "P = (2 x 10$^-$$^1$$^2$)kT$^4$ - 59.637   R$^2$ = 0.9423", fontsize=15)
     plt.savefig("power" + ".pdf")
 
 
@@ -38,16 +57,20 @@ def wien(dataPath):
 
     tempInv = []
     for val in temps:
-        tempInv.append(1/val)
+        tempInv.append(1 / val)
+
+    a, b = best_fit(tempInv, peakW)
+    yfit = [a + b * xi for xi in tempInv]
 
     fig, ax = plt.subplots(nrows=1)
     fig.set_figheight(height)
     fig.set_figwidth(width)
     ax.set_xlabel("Temperature ($K^-$$^1$)")
     ax.set_ylabel("Peak Wavelength (nm)")
-    ax.plot(tempInv, peakW)
+    ax.scatter(tempInv, peakW)
+    ax.plot(tempInv, yfit)
+    ax.text(0.00026, 705, "\u03BB = 369.79 + 978636.37$T^-$$^1$    $R^2$ = 0.8787 ", fontsize=15)
     plt.savefig("wien" + ".pdf")
 
-
 Power(path)
-wien(path)
+#wien(path)
